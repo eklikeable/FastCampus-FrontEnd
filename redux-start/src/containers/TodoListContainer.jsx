37b2a24284d231cc521e48891fe315586c4e6ff6
ekkim <eklikeable@kakao.com>
2022-10-11 21:39:42 +0900

    Redux를 React에 연결 with Hooks

--- redux-start/src/containers/TodoListContainer.jsx
+++ redux-start/src/containers/TodoListContainer.jsx
@@ -1,19 +1,9 @@
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import TodoList from '../components/TodoList';
 
-const mapStateToProps = (state) => {
-    return {
-        todos: state.todos,
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return {};
-};
+function TodoListContainer() {
+    const todos = useSelector((state) => state.todos);
 
-// connect()를 실행한 결과가 함수이므로, ()를 한번 더 써줘야 함
-const TodoListContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(TodoList);
-
+    return <TodoList todos={todos} />;
+}
 export default TodoListContainer;
