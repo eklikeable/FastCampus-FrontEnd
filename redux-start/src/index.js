e1c63e2dce7d3eb3ae47c8ec33c38ae7f426482e
ekkim <eklikeable@kakao.com>
2022-10-11 21:00:01 +0900

    Redux를 React에 연결 without react-redux

--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
-import { addTodo, completeTodo, showComplete } from './redux/actions';
+import ReduxContext from './contexts/ReduxContext';
+// import { addTodo, completeTodo, showComplete } from './redux/actions';
 
 //store에 변경사항이 생기는것을 구독 (unsubscribe하는 함수가 리턴됨)
-store.subscribe(() => {
-    console.log(store.getState());
-});
-store.dispatch(addTodo('coding'));
-store.dispatch(addTodo('reding books'));
-store.dispatch(addTodo('eat dinner'));
-store.dispatch(completeTodo(0));
-store.dispatch(showComplete());
+// store.subscribe(() => {
+//     console.log(store.getState());
+// });
+// store.dispatch(addTodo('coding'));
+// store.dispatch(addTodo('reding books'));
+// store.dispatch(addTodo('eat dinner'));
+// store.dispatch(completeTodo(0));
+// store.dispatch(showComplete());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <ReduxContext.Provider value={store}>
+            <App />
+        </ReduxContext.Provider>
     </React.StrictMode>
 );
